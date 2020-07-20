292703abade0b16b81bc2a8fcbfec97d4e8a3a9c
Naval Kulkarni <navalkulkarni@gmail.com>
2020-07-20 19:43:54 +0530

    add click listerners

--- 1.2-InterActiveUI/app/src/main/java/com/android/hellotoast/MainActivity.kt
+++ 1.2-InterActiveUI/app/src/main/java/com/android/hellotoast/MainActivity.kt
@@ -2,10 +2,28 @@ package com.android.hellotoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0;
+    lateinit var countTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        countTextView = findViewById(R.id.show_count)
+    }
+
+    fun countUp(view: View) {
+        count++
+        if(count!=null)
+            countTextView.text = count.toString()
+    }
+    fun showToast(view: View) {
+
+        Toast.makeText(this,"Hello Toast",Toast.LENGTH_SHORT).show()
+
     }
 }
