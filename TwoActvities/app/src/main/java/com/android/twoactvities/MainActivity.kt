02f99bd4d5c5a1e841c4803fe692b645cfe419e5
Naval Kulkarni <navalkulkarni@gmail.com>
2020-07-23 16:24:18 +0530

    change MainActivity.kt to expect for intent result and show reply from SecondActivity.kt

--- TwoActvities/app/src/main/java/com/android/twoactvities/MainActivity.kt
+++ TwoActvities/app/src/main/java/com/android/twoactvities/MainActivity.kt
@@ -1,24 +1,42 @@
 package com.android.twoactvities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    val REQUEST_CODE = 123
     lateinit var editText: EditText
+    lateinit var replyHeader: TextView
+    lateinit var replyText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         editText = findViewById(R.id.sendButtoneditText)
+        replyHeader = findViewById(R.id.text_reply_header)
+        replyText = findViewById(R.id.text_reply_message)
     }
 
     fun launchSecondActivity(view: View) {
         val intent = Intent(this,SecondActivity::class.java)
         intent.putExtra("Message",editText.text.toString())
-        startActivity(intent)
+        startActivityForResult(intent,REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE)
+            if(resultCode == Activity.RESULT_OK){
+                val message = data?.getStringExtra("Reply")
+                replyText.text = message
+                replyHeader.visibility = View.VISIBLE
+                replyText.visibility = View.VISIBLE
+            }
     }
 }
--- TwoActvities/app/src/main/java/com/android/twoactvities/SecondActivity.kt
+++ TwoActvities/app/src/main/java/com/android/twoactvities/SecondActivity.kt
@@ -1,12 +1,15 @@
 package com.android.twoactvities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,10 @@ class SecondActivity : AppCompatActivity() {
     }
 
     fun launchReply(view: View) {
-
+        val editText = findViewById<EditText>(R.id.replyButtoneditText)
+        val intent = Intent()
+        intent.putExtra("Reply",editText.text.toString())
+        setResult(Activity.RESULT_OK,intent)
+        finish()
     }
 }
