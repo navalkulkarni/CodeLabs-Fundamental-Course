32fe0a8add7d69b9a7fe95aba2938ba878cd800c
Naval Kulkarni <navalkulkarni@gmail.com>
2020-07-23 15:09:42 +0530

    create first activity and add button and edittext to it for sending a message

--- TwoActvities/app/src/main/java/com/android/twoactvities/MainActivity.kt
+++ TwoActvities/app/src/main/java/com/android/twoactvities/MainActivity.kt
@@ -2,10 +2,20 @@ package com.android.twoactvities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.sendButtoneditText)
+    }
+
+    fun launchSecondActivity(view: View) {
+
     }
 }
