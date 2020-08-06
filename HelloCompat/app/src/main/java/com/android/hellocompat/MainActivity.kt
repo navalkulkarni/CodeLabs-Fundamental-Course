99ca74e2a5cdf0d01fb9714de42c3dfd3bb67b1c
Naval Kulkarni <navalkulkarni@gmail.com>
2020-08-06 17:09:32 +0530

    complete layout of MainActivity.kt

--- HelloCompat/app/src/main/java/com/android/hellocompat/MainActivity.kt
+++ HelloCompat/app/src/main/java/com/android/hellocompat/MainActivity.kt
@@ -2,10 +2,13 @@ package com.android.hellocompat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun changeColor(view: View) {}
 }
