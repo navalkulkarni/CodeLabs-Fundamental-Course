79196a9a281b6625bced2182ea91a62d20e820a0
Naval Kulkarni <navalkulkarni@gmail.com>
2020-08-07 18:10:01 +0530

    add click listeners to FirstFragment.kt

--- DroidCafe/app/src/main/java/com/android/droidcafe/FirstFragment.kt
+++ DroidCafe/app/src/main/java/com/android/droidcafe/FirstFragment.kt
@@ -1,17 +1,19 @@
 package com.android.droidcafe
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +26,27 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        donut.setOnClickListener {
+            displayToast(getString(R.string.donut_order_message))
+        }
+
+        froyo.setOnClickListener {
+            displayToast(getString(R.string.froyo_order_message))
+        }
+
+        ice_cream.setOnClickListener {
+            displayToast(getString(R.string.ice_cream_order_message))
+        }
     }
+
+    fun displayToast(message: String?) {
+        Toast.makeText(
+            context, message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+
+
+
 }
