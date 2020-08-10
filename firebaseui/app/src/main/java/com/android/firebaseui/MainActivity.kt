e20f2aa0c7bece3093ee14be20edb1ee6ecf75fb
Naval Kulkarni <navalkulkarni@gmail.com>
2020-08-10 16:48:05 +0530

    add login button to MainActivity and add firebase dependency

--- firebaseui/app/src/main/java/com/android/firebaseui/MainActivity.kt
+++ firebaseui/app/src/main/java/com/android/firebaseui/MainActivity.kt
@@ -2,10 +2,16 @@ package com.android.firebaseui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loginButton = findViewById(R.id.loginButton)
+
     }
 }
