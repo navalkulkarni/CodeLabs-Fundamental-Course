6a30a72ea8c0a40e4b92724b388fe3881d9ed68b
Naval Kulkarni <navalkulkarni@gmail.com>
2020-08-24 16:35:28 +0530

    add spinner to SecondFragment.kt and take its input as selected item.

--- DroidCafe/app/src/main/java/com/android/droidcafe/SecondFragment.kt
+++ DroidCafe/app/src/main/java/com/android/droidcafe/SecondFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.*
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_second.*
@@ -13,7 +13,7 @@ import kotlinx.android.synthetic.main.fragment_second.*
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SecondFragment : Fragment(),AdapterView.OnItemSelectedListener {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -31,5 +31,31 @@ class SecondFragment : Fragment() {
             val result = bundle.getString("bundleKey")
             order_textview.text = result
         }
+
+        val spinner = view.findViewById<Spinner>(R.id.label_spinner)
+        if(spinner!=null)
+            spinner.onItemSelectedListener = this
+
+        val adapter = context?.let { ArrayAdapter.createFromResource(it,R.array.labels_array,R.layout.support_simple_spinner_dropdown_item) }
+
+        if (adapter != null) {
+            adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+        }
+
+        if(spinner!=null)
+            spinner.adapter = adapter
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val spinnerLabel = p0?.getItemAtPosition(p2).toString()
+        displayToast(spinnerLabel)
+    }
+
+    private fun displayToast(spinnerLabel: String) {
+        Toast.makeText(context,spinnerLabel,Toast.LENGTH_SHORT).show()
     }
 }
